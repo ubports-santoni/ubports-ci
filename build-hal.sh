26be111220b332df1ef86ec13d5465d8d8fc2afd
BirdZhang <0312birdzhang@gmail.com>
2019-12-11 21:45:11 +0800

    some fixes

--- build-hal.sh
+++ build-hal.sh
@@ -8,6 +8,6 @@ sed -i 's/external\/selinux/external\/selinux external\/libcurl/g' build/core/ma
 source build/envsetup.sh
 export USE_CCACHE=1
 breakfast $DEVICE
-mka hybris-hal
-mka halium-boot
-mka systemimage 
+make -j$(nproc) hybris-hal
+make -j$(nproc) halium-boot
+make -j$(nproc) systemimage 
