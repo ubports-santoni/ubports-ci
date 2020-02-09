fdbec3cbaec930fcbc85ac50708a2703acdf56b7
Danct12 <danct12@disroot.org>
2020-02-10 02:25:48 +0700

    New local_manifest integration. Thanks NotKit!
    Signed-off-by: Danct12 <danct12@disroot.org>

--- build-hal.sh
+++ build-hal.sh
@@ -2,15 +2,14 @@
 source halium.env
 cd $ANDROID_ROOT
 
-# replace something
-sed -i 's/external\/selinux/external\/selinux external\/libcurl/g' build/core/main.mk
-
 source build/envsetup.sh
 export USE_CCACHE=1
 breakfast $DEVICE
 make -j$(nproc) halium-boot
+make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
 md5sum $ANDROID_ROOT/out/target/product/santoni/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/santoni/recovery.img
 md5sum $ANDROID_ROOT/out/target/product/santoni/system.img
