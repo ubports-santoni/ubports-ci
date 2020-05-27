692471b7cf5d2f6458f7612735598fd33479e5a1
Danct12 <danct12@dreemurrs-private-network.local>
2020-05-27 12:09:57 +0700

    Modified for building Xiaomi Redmi Note 7 (lavender)
    Signed-off-by: Danct12 <danct12@dreemurrs-private-network.local>

--- build-hal.sh
+++ build-hal.sh
@@ -5,11 +5,10 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export USE_CCACHE=1
 breakfast $DEVICE
+make -j$(nproc) fec
 make -j$(nproc) halium-boot
-make -j$(nproc) recoveryimage
 make -j$(nproc) systemimage 
 
 echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/santoni/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/santoni/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/santoni/system.img
+md5sum $ANDROID_ROOT/out/target/product/lavender/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/lavender/system.img
